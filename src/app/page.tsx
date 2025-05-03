7e05d0d5dfaa91b2e0fbc3dd5ca54490eedef2fc
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-05-03 05:15:07 +0100

    fix: adjust header padding in HomePage for improved layout consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import MapView from "./components/MapView";
 const HomePage = () => {
   return (
     <main className="h-dvh w-full">
-      <header className="text-white bg-black/70 text-center py-5 absolute right-0 left-0 z-50">
+      <header className="text-white bg-black/70 text-center py-1.5 absolute right-0 left-0 z-50">
         <p>Flight Tracker App</p>
       </header>
       <MapView />
