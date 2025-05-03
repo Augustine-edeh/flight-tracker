7a3502a48e5ae843342eb08411d843967b9c23e7
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-05-03 03:54:01 +0100

    Chore: update MapView component to enhance flight info display and clean up commented code

--- src/app/components/MapView.tsx
+++ src/app/components/MapView.tsx
@@ -9,15 +9,28 @@ const MapView = () => {
         center={[51.505, -0.09]}
         zoom={13}
         scrollWheelZoom={false}
-        style={{ height: "100%", width: "100%" }} // add this!
+        style={{ height: "100%", width: "100%" }}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker position={[51.505, -0.09]}>
-          <Popup>
+          {/* <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup> */}
+          <Popup>
+            ✈️ <strong>Flight Info</strong>
+            <br />
+            Airline: Demo Air
+            <br />
+            Flight: DA123
+            <br />
+            Altitude: 32,000 ft
+            <br />
+            Speed: 550 knots
+            <br />
+            Status: En Route
           </Popup>
         </Marker>
       </MapContainer>
