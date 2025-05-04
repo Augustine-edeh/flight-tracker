bdd9ff8a155105504fa03bebb59990178d024331
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-05-04 05:10:44 +0100

    chore(zoom-ui): reposition the ZoomControl to bottom-right of the map for improved map interaction

--- src/app/components/MapView.tsx
+++ src/app/components/MapView.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  ZoomControl,
+} from "react-leaflet";
 import { aircraftIcon } from "@/lib/fixLeafletIcon"; // Assuming you have a custom icon for the aircraft
 
 import useAircraftStore from "../stores/aircraftStore"; // Import the Zustand store
@@ -45,6 +51,7 @@ const MapView = () => {
         zoom={5}
         scrollWheelZoom={true}
         style={{ height: "100%", width: "100%", zIndex: "40" }}
+        zoomControl={true}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -66,6 +73,8 @@ const MapView = () => {
               </Marker>
             )
         )} */}
+
+        <ZoomControl position="bottomright" />
       </MapContainer>
     </div>
   );
