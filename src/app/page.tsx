83d4d34c8b500a6baadee61cb4d39b22ec9dfb4b
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-05-05 01:47:32 +0100

    refactor: remove commented header from HomePage component for cleaner code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,6 @@ const MapView = dynamic(() => import("./components/MapView"), {
 const HomePage = () => {
   return (
     <main className="h-dvh w-full">
-      {/* <header className="text-white bg-black/70 text-center py-1.5 absolute right-0 left-0 z-50">
-        <p>Flight Tracker App</p>
-      </header> */}
-
       <Header />
 
       <MapView />
