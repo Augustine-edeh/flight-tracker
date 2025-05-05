a1454adeec57b99623ec8fe2ffd89a77b90208c7
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-05-05 01:46:44 +0100

    fix(build-error): dynamically import MapView component for improved performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import Header from "./components/Header";
-import MapView from "./components/MapView";
+
+import dynamic from "next/dynamic";
+
+const MapView = dynamic(() => import("./components/MapView"), {
+  ssr: false,
+});
 
 const HomePage = () => {
   return (
