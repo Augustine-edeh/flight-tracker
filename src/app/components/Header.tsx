537f42b908e8f0d64508a3d26bf406eb0e90a68c
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-05-21 00:36:42 +0100

    fix(Header): revert background gradient to solid color for header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="absolute top-0 left-0 right-0 bg-gradient-to-b from-black/80 via-black/70 to-transparent text-white py-2 z-50 text-center">
+    <header className="absolute top-0 left-0 right-0 bg-[#273e56] text-white py-2 z-50 text-center">
       <h1 className="text-2xl font-semibold">Flight Tracker</h1>
     </header>
   );
