eca469229bef58d12c509dc3c76a24450df5002d
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-05-20 16:44:53 +0100

    feat(HomePage): add clsx for dynamic class management in error banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import useAircraftStore from "./stores/aircraftStore"; // Import the Zustand store
 
 import dynamic from "next/dynamic";
+import clsx from "clsx";
 
 const MapView = dynamic(() => import("./components/MapView"), {
   ssr: false,
@@ -15,11 +16,20 @@ const HomePage = () => {
 
   const { error } = useAircraftStore();
 
+  const shouldShowBanner = error && showError;
+
   return (
     <main className="flex flex-col h-dvh w-full">
-      <div className="absolut e flex justify-between top-0 left-0 right-0 z-[1000] bg-red-500 text-white px-4 py -2 shadow-md">
-        {error && showError && (
-          <>
+      <div
+        className={clsx(
+          "top-0 left-0 right-0 z-[1000] px-4 py- 2 shadow-md transition-all duration-500 ease-in-out bg-red-500",
+          shouldShowBanner
+            ? "bg-blue- 500 text-white opacity-100 translate-y-0"
+            : "opacity-0 -translate-y-full pointer-events-none"
+        )}
+      >
+        {shouldShowBanner && (
+          <div className="flex justify-between items-center">
             <p className="text-sm font-medium truncate">⚠️ {error}</p>
             <button
               onClick={() => setShowError(false)}
@@ -27,7 +37,7 @@ const HomePage = () => {
             >
               ✕
             </button>
-          </>
+          </div>
         )}
       </div>
 
