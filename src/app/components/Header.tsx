d4d0101a811c34e8a82ec8c86e94996ea46abe5a
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-05-21 01:28:01 +0100

    feat(Header): add search icon with lucide-react icon

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,45 @@
+"use client";
+
+import { useState, useRef, useEffect } from "react";
+import { Search } from "lucide-react";
+
 const Header = () => {
+  const [showSearch, setShowSearch] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (showSearch && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [showSearch]);
+
   return (
-    <header className="absolute top-0 left-0 right-0 bg-[#273e56] text-white py-2 z-50 text-center">
-      <h1 className="text-2xl font-semibold">Flight Tracker</h1>
+    <header className="absolute top-0 left-0 right-0 bg-[#273e56] text-white py-2 z-50 px-4 flex justify-between items-center">
+      <h1 className="text-2xl font-semibold text-center flex-1">
+        Flight Tracker
+      </h1>
+
+      <div className="flex items-center space-x-2 relative">
+        <div
+          className={`transition-all duration-300 ease-in-out flex items-center overflow -hidden ${
+            showSearch ? "w-48 opacity-100 mr-2" : "w-0 opacity-0"
+          }`}
+        >
+          <input
+            ref={inputRef}
+            type="text"
+            placeholder="Search..."
+            className="bg-white text-black px-3 py-1 rounded-md w-full text-sm outline-1 outline-green-400"
+          />
+        </div>
+
+        <button
+          onClick={() => setShowSearch((prev) => !prev)}
+          className="text-white hover:text-gray-300 transition cursor-pointer"
+        >
+          <Search className="size-5" />
+        </button>
+      </div>
     </header>
   );
 };
